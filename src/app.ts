8a53a54ba4a0a399abdb9168b4a5c620a1319461
TGzin66 <heron07emanuel@gmail.com>
2025-09-27 21:05:59 -0300

    resolver post no app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import { userRouter } from "./routes/userRouter";
+import { postRouter } from "./routes/postRouter";
 
-export const app = express()
+export const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-app.listen(3003, () => {
-    console.log('Servidor rodando na porta 3003')
-})
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
